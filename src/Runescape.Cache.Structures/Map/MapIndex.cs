847798e6e0256f40dffe21012552076ed58d8882
HelloKitty <andrew.blakely@ymail.com>
2017-06-10 12:20:52 -0500

    Add MapIndex and RegionInformation

---
+++ src/Runescape.Cache.Structures/Map/MapIndex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Runescape.Cache
+{
+	/// <summary>
+	/// Collection of Region information.
+	/// Found in map_index in the Runescape cache.
+	/// </summary>
+	[WireDataContract]
+	public sealed class MapIndex : IEnumerable<RegionInformation>
+	{
+		/// <summary>
+		/// Deserialized region information.
+		/// </summary>
+		[WireMember(1)]
+		public RegionInformation[] _Regions { get; }
+
+		/// <summary>
+		/// Collection of the map index' region information.
+		/// </summary>
+		public IEnumerable<RegionInformation> Regions => _Regions;
+
+		/// <summary>
+		/// Protected ctor for serialization.
+		/// </summary>
+		protected MapIndex()
+		{
+			
+		}
+
+		/// <inheritdoc />
+		public IEnumerator<RegionInformation> GetEnumerator()
+		{
+			return Regions.GetEnumerator();
+		}
+
+		/// <inheritdoc />
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ src/Runescape.Cache.Structures/Map/RegionInformation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Runescape.Cache
+{
+	[WireDataContract]
+	public sealed class RegionInformation
+	{
+		/*regionHash[reg] = b.getShortUnsigned();
+		regionLandscapeIndex[reg] = b.getShortUnsigned();
+		regionObjectMapIndex[reg] = b.getShortUnsigned();
+		regionPreload[reg] = b.getUnsigned();*/
+
+		/// <summary>
+		/// Location hash for the region.
+		/// </summary>
+		[WireMember(1)]
+		public ushort RegionHash { get; }
+
+		//TODO: What is this?
+		/// <summary>
+		/// ?
+		/// </summary>
+		[WireMember(2)]
+		public ushort RegionLandscapeIndex { get; }
+
+		//TODO: What is this?
+		/// <summary>
+		/// ?
+		/// </summary>
+		[WireMember(3)]
+		public ushort RegionObjectMapIndex { get; }
+
+		//TODO: What is this?
+		/// <summary>
+		/// ?
+		/// </summary>
+		[WireMember(4)]
+		public byte RegionPreload { get; }
+
+		/// <summary>
+		/// Protected serializer ctor
+		/// </summary>
+		protected RegionInformation()
+		{
+			
+		}
+	}
+}
