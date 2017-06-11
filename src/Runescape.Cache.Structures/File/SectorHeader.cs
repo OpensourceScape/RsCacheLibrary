891323e6f33fcca544c3e4a7f2b6f72a435fcd5b
HelloKitty <andrew.blakely@ymail.com>
2017-06-11 16:29:58 -0500

    Added SectorHeader

---
+++ src/Runescape.Cache.Structures/File/SectorHeader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using Runescape.Cache.Structures.Common;
+
+namespace Runescape.Cache.Structures
+{
+	/// <summary>
+	/// Index entry contained within index files.
+	/// </summary>
+	[WireDataContract]
+	public sealed class SectorHeader
+	{
+		/// <summary>
+		/// Represents the size of the sector.
+		/// </summary>
+		[WireMember(1)]
+		public UInt24 SectorSize { get; }
+
+		/// <summary>
+		/// Represents the starting point of the sector.
+		/// </summary>
+		[WireMember(2)]
+		public UInt24 SectorStartPosition { get; }
+
+		/// <summary>
+		/// Protected serializer ctor.
+		/// </summary>
+		protected SectorHeader()
+		{
+			
+		}
+	}
+}
